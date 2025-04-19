a7c38002cd283a68e8e592eae321374b055135f0
Felipe <felipesfc2016@gmail.com>
2025-04-19 10:37:17 -0300

    first commit

--- app.py
+++ app.py
@@ -13,20 +13,21 @@ def validar_login():
    else:
        login_feito.configure(text="Login ou senha inválidos", text_color="red")
 
-
+# Definindo a janela principal
 app = ctk.CTk()
 app.title("sistema de login")
 app.geometry("400x300")
 
+# Definindo o título 
 label_Usuario = ctk.CTkLabel(app,text="Usuário:")
 label_Usuario.pack(pady=10)
-
+# Definindo o campo de entrada do usuário
 Campo_usuario = ctk.CTkEntry(app,placeholder_text="Digite seu usuário")
 Campo_usuario.pack(pady=10)
-
+# Definindo o título de entrada da senha
 label_Senha = ctk.CTkLabel(app,text="Senha:")
 label_Senha.pack(pady=10)
-
+# Definindo o campo de entrada da senha
 Campo_Senha = ctk.CTkEntry(app,placeholder_text="Digite sua senha",show="*")
 Campo_Senha.pack(pady=10)
 

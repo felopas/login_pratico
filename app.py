8dfe4152448cdaf7b67580826155a93b5ae41964
Felipe <felipesfc2016@gmail.com>
2025-04-19 10:25:55 -0300

    janela de login com ctkcustom

---
+++ app.py
@@ -0,0 +1,38 @@
+import customtkinter as ctk # type: ignore
+
+## Configurações do tema
+ctk.set_appearance_mode("dark")
+
+#definindo a função de validação de login
+def validar_login():
+   Usuario = Campo_usuario.get()
+   senha = Campo_Senha.get()
+
+   if Usuario == "adm" and senha == "123":
+       login_feito.configure(text="Login feito com sucesso", text_color="green")
+   else:
+       login_feito.configure(text="Login ou senha inválidos", text_color="red")
+
+
+app = ctk.CTk()
+app.title("sistema de login")
+app.geometry("400x300")
+
+label_Usuario = ctk.CTkLabel(app,text="Usuário:")
+label_Usuario.pack(pady=10)
+
+Campo_usuario = ctk.CTkEntry(app,placeholder_text="Digite seu usuário")
+Campo_usuario.pack(pady=10)
+
+label_Senha = ctk.CTkLabel(app,text="Senha:")
+label_Senha.pack(pady=10)
+
+Campo_Senha = ctk.CTkEntry(app,placeholder_text="Digite sua senha",show="*")
+Campo_Senha.pack(pady=10)
+
+ctk.CTkButton(app,text="login",command=validar_login).pack(pady=10)
+
+login_feito = ctk.CTkLabel(app,text="")
+login_feito.pack(pady=10)
+
+app.mainloop()
